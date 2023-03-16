2f3a8ddcbebf7c9f9f2316c897b943aff8e87ff8
Romerodani23 <2099dani0@gmail.com>
2023-03-16 10:02:00 -0500

    arreglo js

--- main.js
+++ main.js
@@ -7,21 +7,21 @@
 
 
 
-let = 2,1,3
+let correctas = [2,1,3];
 
 let opcion_elegido=[];
 let cantidad_correctas=0;
 
 function respuesta(num_pregunta, seleccionada){
-    opcion_elegida[num_pregunta] = seleccionada.value;
+    opcion_elegido[num_pregunta] = seleccionada.value;
 
     id="p" + num_pregunta;
 
 
     labels = document.getElementByid(id).childNodes;
-    labels[2].style.backgroundColor = "white";
-    labels[1].style.backgroundColor = "white";
     labels[3].style.backgroundColor = "white";
+    labels[5].style.backgroundColor = "white";
+    labels[7].style.backgroundColor = "white";
 
 
     seleccionada.parentNode.style.backgroundColor = "#cec0fc";
@@ -31,8 +31,8 @@ function respuesta(num_pregunta, seleccionada){
 
 function corregir(){
     cantidad_correctas = 0;
-    for(i=0;1 < correctas.length;i++){
-        if(correctas[1]==opcion_elegida[1]){
+    for(i=0;i < correctas.length;i++){
+        if(correctas[i]==opcion_elegido[i]){
             cantidad_correctas++;
         }
 
